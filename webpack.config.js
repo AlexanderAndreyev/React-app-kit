d86b9b250a59b2db0f7bb1dce518045c18e585f0
Alexander <alexandr210993@gmail.com>
2017-04-13 13:40:40 +0300

    Initial commit. Babel config and basic webpack config were added.

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: ['babel-polyfill', './index.js']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/public/'
+  },
+  resolve: {
+    modules: ['node_modules']
+  },
+  module: {
+    rules: [
+      {
+         test: /\.js$/,
+         exclude: /node_modules/,
+         use: 'babel-loader'
+       },
+      {
+        test:/\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test:/\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test:/\.(jpe?g|png|gif|svg)$/i,
+        use: 'file-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    port: 3000,
+    stats: "errors-only",
+    open: true,
+  }
+}
